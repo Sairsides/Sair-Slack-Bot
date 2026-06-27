3879a9091985cd061f76f929691af856572d8f47
Sairsides <your-github-email@example.com>
2026-06-27 19:11:16 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: 'tokens.env' });
+
+const axios = require("axios");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/sairbot-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/sairbot-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+
+    await respond({ text: "⚠️ The cat fact service is temporarily offline. Please try again later." });
+  }
+});
